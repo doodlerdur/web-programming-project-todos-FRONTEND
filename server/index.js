87c110f137c654a37eaf8c912d0a2149ae44d6dc
Ville Kahelin <t3kavi02@students.oamk.fi>
2024-03-25 01:49:14 +0200

    todo_part_4 & todo_part_5 ready

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 const port = 3001
 const openDb = () => {
@@ -39,6 +40,20 @@ app.post("/new", (req,res) => {
         
         } else {
             res.status(200).json({id : result.rows[0].id})
+        }
+    })
+})
+
+app.delete("/delete/:id",async(req,res)=> {
+    const pool = openDb()
+    const id = parseInt(req.params.id)
+    pool.query('delete from task where id = $1',
+    [id],
+    (error,result) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        } else {
+            res.status(200).json({id:id})
         }
     })
 })
