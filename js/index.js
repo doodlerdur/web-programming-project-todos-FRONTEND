7afed5ff9784352fd7fab212ae2e04c3abf4bf32
Ville Kahelin <t3kavi02@students.oamk.fi>
2024-03-24 03:08:11 +0200

    todo_part_3 ready

--- js/index.js
+++ js/index.js
@@ -1,30 +1,60 @@
-// References to UI elements
+const BACKEND_ROOT_URL = 'http://localhost:3001'
+
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
 
-// Event listener for the input field
-input.addEventListener('keypress',(event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault()
-        
-        // Get's the value from the input field
-        const task = input.value.trim()
+input.disabled = true
 
-        // Check if the input field is empty
-        if (task !== '') {
 
-            // Create a new list item
+        const renderTask = (task) => {
             const li = document.createElement('li')
-
-            // Set the innerHTML of the list item to the task text
             li.setAttribute('class','list-group-item')
             li.innerHTML = task
-
-            // Append the list item to the list
             list.append(li)
+        }
 
-            // Clear the input field
-            input.value = ''
+        const getTasks = async () => {
+            try {
+                const response = await fetch(BACKEND_ROOT_URL)
+                const json = await response.json()
+                json.forEach(task => {
+                    renderTask(task.description)
+                })
+                input.disabled = false
+              } catch (error) {
+                alert("Error retrieving tasks " + error.message)
+              }
+        }
+
+        const saveTask = async (task) => {
+            try {
+                const json = JSON.stringify({description: task})
+                const response = await fetch(BACKEND_ROOT_URL + '/new',{
+                    method: 'post',
+                    headers: {
+                        'Content-Type':'application/json'
+                    },
+                    body: json
+                })
+                return response.json()
+              } catch (error) {
+                alert("Error saving task" + error.message)
+              }
+            }
+
+            input.addEventListener('keypress',(event) => {
+                if (event.key === 'Enter') {
+                    event.preventDefault()
+                    const task = input.value.trim()
+                    if (task !== '') {
+                        saveTask(task).then((json)=> {
+                            renderTask(task)
+                            input.value = ''
+                        
+
+            })
         }
     }
 })
+
+getTasks()
--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ app.use(cors())
 app.use(express.json())
 
 const port = 3001
-
 const openDb = () => {
     const pool = new Pool ({
         user: 'postgres',
@@ -18,15 +17,26 @@ const openDb = () => {
     })
     return pool
 }
+app.get("/",(req,res) => {
+    const pool = openDb()
 
-app.get("/pg", (req, res) => {
+    pool.query('SELECT * FROM task', (error, result) => {
+        if ( error ) {
+            res.status(500).json({ error: error.message })
+        }
+        res.status(200).json(result.rows)
+    })
+})
+
+app.post("/new", (req,res) => {
     const pool = openDb()
 
     pool.query('insert into task (description) values ($1) returning *',
     [req.body.description],
-    (error, result) => {
+    (error,result) => {
         if (error) {
             res.status(500).json({error: error.message})
+        
         } else {
             res.status(200).json({id : result.rows[0].id})
         }
